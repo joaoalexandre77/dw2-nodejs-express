a6c65b9ce83114a82067c17059e921c57beb681a
joao <joao.armax17@gmail.com>
2026-02-11 20:52:48 -0300

    variavel

---
+++ js-aula-01-tipos-de-funcoes/variaveis/script/main.js
@@ -0,0 +1,13 @@
+//Var: No geral, evite o seu uso.
+//Let: Utilize quando for necessário reatribuir o valor da variável.
+//Const: Utilize quando não precisar reatribuir o valor da variável.
+
+var cidade = "Jacupiranga";
+var cidade = "Registro";
+
+let nome = "João";
+nome = "João";
+
+const pais = "Brasil";
+document.write(`${cidade} - ${pais} - ${nome}`);
+
